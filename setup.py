e91815028d96c847908059314309c4df3cb4013b
Adrian Fluder <afluder0147@gmail.coim>
2024-07-14 16:19:31 +0200

    setup

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ import subprocess
 import sys
 import pkg_resources
 
+
 def install_package(package_name):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+    subprocess.check_call([sys.executable,
+                          "-m", "pip", "install",
+                           package_name])
+
 
 def is_package_installed(package_name):
     try:
@@ -12,10 +16,16 @@ def is_package_installed(package_name):
     except pkg_resources.DistributionNotFound:
         return False
 
-with open('requirements.txt', 'r') as file:
-    for line in file:
-        package_name = line.strip()
-        if not is_package_installed(package_name):
-            install_package(package_name)
+
+def setup(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            package_name = line.strip()
+            if not is_package_installed(package_name):
+                install_package(package_name)
+
+
+setup("requirements.txt")
+setup("requirements-dev.txt")
 
 print("setup finished")
