2a5d6deb562c7f0a380838e47dc02af21a009a6d
Adrian Fluder <afluder0147@gmail.coim>
2024-07-14 16:03:47 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from deepl import core
+import subprocess
+import sys
+import pkg_resources
 
-core.test_function()
+def install_package(package_name):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+
+def is_package_installed(package_name):
+    try:
+        pkg_resources.get_distribution(package_name)
+        return True
+    except pkg_resources.DistributionNotFound:
+        return False
+
+with open('requirements.txt', 'r') as file:
+    for line in file:
+        package_name = line.strip()
+        if not is_package_installed(package_name):
+            install_package(package_name)
+
+print("setup finished")
