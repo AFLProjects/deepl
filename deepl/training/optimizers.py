fe443813bc8252e2198dd83e5065b8506779a0e3
Adrian Fluder <afluder0147@gmail.coim>
2024-07-16 13:07:11 +0200

    cleanup

--- deepl/training/optimizers.py
+++ deepl/training/optimizers.py
@@ -5,6 +5,7 @@ from utils import time_perf
                                                   
 
 class SGD_Optimizer:
+
     @time_perf
     def __init__(self, nn, loss=mse,
                 initialization=uniform_init, initialization_args=(0,1), 
@@ -15,22 +16,23 @@ class SGD_Optimizer:
         self.loss_values = []  # Store loss values for each iteration
         self.initialization = initialization  # Method to initialize weights
         self.initialization_args = initialization_args  # Arguments for initialization
-        self.current_w_tensor = initialization(nn.structure,
-                                                  *initialization_args)  # Initialize weights
+        self.current_w_tensor = initialization(nn.structure, *initialization_args)  # Initialize weights
         self.start_learning_rate = start_learning_rate  # Initial learning rate
         self.learning_rate = self.start_learning_rate  # Current learning rate
         self.callbacks = callbacks  # List of callback functions
 
+
+    # Reset weights, loss values, and learning rate
     def reset(self):
-        # Reset weights, loss values, and learning rate
         self.loss_values = []
         self.current_w_tensor = self.initialization(self.nn.structure,
                                                   *self.initialization_args)
         self.learning_rate = self.start_learning_rate
         
+    
+    # Train the neural network using SGD for a specified number of iterations
     @time_perf
     def train(self, x, y, data_size):
-        # Train the neural network using SGD for a specified number of iterations
         for i in range(data_size):
             gradient = self.grad_loss(self.current_w_tensor, self.nn, x[i], y[i])
             loss_value = self.loss(self.current_w_tensor, self.nn, x[i], y[i])
